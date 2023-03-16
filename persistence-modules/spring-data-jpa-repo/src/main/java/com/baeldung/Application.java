8bd4b54ac9caa902744527277b7d12449d67bff4
Patrice Torguet <torguet@gmail.com>
2023-03-16 17:14:38 +0100

    Adding a secret.properties file to hold MySQL credentials.

--- persistence-modules/spring-data-jpa-repo/src/main/java/com/baeldung/Application.java
+++ persistence-modules/spring-data-jpa-repo/src/main/java/com/baeldung/Application.java
@@ -2,11 +2,13 @@ package com.baeldung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.baeldung.boot.daos.impl.ExtendedRepositoryImpl;
 
 @SpringBootApplication
+@PropertySource("classpath:secret.properties")
 @EnableJpaRepositories(repositoryBaseClass = ExtendedRepositoryImpl.class)
 public class Application {
 
